3ccf8c272e03be8b72a18d9ed8e02224dddd5d74
awmxr <amir.m.n.1380@gmail.com>
2025-03-04 21:23:37 +0330

    PersistenceServicesRegistration

---
+++ HR_Management.Persistence/PersistenceServicesRegistration.cs
@@ -0,0 +1,34 @@
+﻿using HR_Management.Application.Persistence.Contract;
+using HR_Management.Persistence.Repository;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR_Management.Persistence
+{
+    public static class PersistenceServicesRegistration
+    {
+        public static IServiceCollection ConfigurePersistenceServices(this IServiceCollection services , IConfiguration configuration)
+        {
+
+            services.AddDbContext<LeaveManagementDbContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("LeaveManagementConnectionString"));
+            });
+
+            services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+
+            services.AddScoped<ILeaveRequestRepository, LeaveRequestRepository>();
+            services.AddScoped<ILeaveTypeRepository, LeaveTypeRepository>();
+            services.AddScoped<ILeaveAllocationRepository, LeaveAllocationRepository>();
+
+            return services;
+
+        }
+    }
+}
