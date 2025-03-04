3017436f958ffc168670bd58bc687443a2b06317
awmxr <amir.m.n.1380@gmail.com>
2025-03-05 01:16:36 +0330

    InfrastructureServicesRegistration And EmailSender Implementation

---
+++ HR_Management.Infrastructure/InfrastructureServicesRegistration.cs
@@ -0,0 +1,23 @@
+﻿using HR_Management.Application.Contract.Infrastructure;
+using HR_Management.Application.Models;
+using HR_Management.Infrastructure.Mail;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR_Management.Infrastructure
+{
+    public static class InfrastructureServicesRegistration
+    {
+        public static IServiceCollection ConfigureInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.Configure<EmailSetting>(configuration.GetSection("EmailSettings"));
+            services.AddTransient<IEmailSender, EmailSender>();
+            return services;
+        }
+    }
+}
