3551099b4aeab087dae8ce881a779697b208999c
awmxr <amir.m.n.1380@gmail.com>
2025-03-04 19:53:14 +0330

    Complete mapping profile

--- HR_Management.Application/Profiles/MappingProfile.cs
+++ HR_Management.Application/Profiles/MappingProfile.cs
@@ -15,10 +15,26 @@ namespace HR_Management.Application.Profiles
     {
         public MappingProfile()
         {
+            #region LeaveRequest Mapping
             CreateMap<LeaveRequest, LeaveRequestDto>().ReverseMap();
             CreateMap<LeaveRequest, LeaveRequestListDto>().ReverseMap();
+            CreateMap<LeaveRequest, CreateLeaveRequestDto>().ReverseMap();
+            CreateMap<LeaveRequest, UpdateLeaveRequestDto>().ReverseMap();
+            #endregion
+
+            #region LeaveAllocation Mapping
             CreateMap<LeaveAllocation, LeaveAllocationDto>().ReverseMap();
+            CreateMap<LeaveAllocation, CreateLeaveAllocationDto>().ReverseMap();
+            CreateMap<LeaveAllocation, UpdateLeaveAllocationDto>().ReverseMap();
+            #endregion
+
+            #region LeaveType Mapping
             CreateMap<LeaveType, LeaveTypeDto>().ReverseMap();
+            CreateMap<LeaveType, CreateLeaveTypeDto>().ReverseMap();
+            #endregion
+
+
+
         }
     }
 }
