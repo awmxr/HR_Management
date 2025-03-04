94cf5d0830424660b5c78cc764d58e3457ff10a2
awmxr <amir.m.n.1380@gmail.com>
2025-03-04 16:55:05 +0330

    add FluentValidation to project Application

---
+++ HR_Management.Application/DTOs/LeaveType/Validators/CreateLeaveTypeDtoValidator.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+
+
+namespace HR_Management.Application.DTOs.LeaveType.Validators
+{
+    public class CreateLeaveTypeDtoValidator : AbstractValidator<CreateLeaveTypeDto>
+    {
+        public CreateLeaveTypeDtoValidator()
+        {
+            RuleFor(p=> p.Name)
+                .NotEmpty().WithMessage("{PropertyName} is required.")
+                .NotNull()
+                .MaximumLength(50).WithMessage("{PropertyName} must not exeed 50.");
+
+            RuleFor(p => p.DefaultDay)
+                .NotEmpty().WithMessage("{PropertyName} is required.")
+                .NotNull()
+                .GreaterThan(0).LessThan(100);
+        }
+    }
+}
