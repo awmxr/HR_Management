ad130a1995548fd7d98e12ee06e2b3b2f5d2b33b
awmxr <amir.m.n.1380@gmail.com>
2025-03-06 21:20:59 +0330

    add identity db

--- HR_Management.Identity/LeaveManagementIdentityDbContext.cs
+++ HR_Management.Identity/LeaveManagementIdentityDbContext.cs
@@ -2,6 +2,7 @@
 using HR_Management.Identity.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,6 +14,16 @@ namespace HR_Management.Identity;
 public class LeaveManagementIdentityDbContext : IdentityDbContext<ApplicationUser>
     
 {
+    public LeaveManagementIdentityDbContext(DbContextOptions<LeaveManagementIdentityDbContext> options)
+        : base(options)
+    {
+        
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.ConfigureWarnings(warnings => warnings.Ignore(RelationalEventId.PendingModelChangesWarning));
+    }
     protected override void OnModelCreating(ModelBuilder builder)
     {
         base.OnModelCreating(builder);
