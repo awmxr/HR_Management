283036e9a567fe9a02bddb46d3a2cef7c40d5342
awmxr <amir.m.n.1380@gmail.com>
2025-03-06 20:34:20 +0330

    IdentityServiceRegistration

---
+++ HR_Management.Identity/IdentityServiceRegistration.cs
@@ -0,0 +1,63 @@
+﻿using HR_Management.Application.Contract.Identity;
+using HR_Management.Application.Models.Identity;
+using HR_Management.Identity.Models;
+using HR_Management.Identity.Serivecs;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR_Management.Identity
+{
+    public static class IdentityServiceRegistration
+    {
+        public static IServiceCollection ConfigureIdentityServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.Configure<JwtSettings>(configuration.GetSection("JwtSettings"));
+            services.AddDbContext<LeaveManagementIdentityDbContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("LeaveManagementConnectionString")
+                    , b => b.MigrationsAssembly(typeof(LeaveManagementIdentityDbContext).Assembly.FullName));
+            });
+
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<LeaveManagementIdentityDbContext>();
+
+            services.AddTransient<IAuthService, AuthService>();
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+
+            })
+                .AddJwtBearer(o =>
+                {
+                    o.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuerSigningKey = true,
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ClockSkew = TimeSpan.Zero,
+                        ValidIssuer = configuration["JwtSettings:Issuer"],
+                        ValidAudience = configuration["JwtSettings:Audience"],
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JwtSettings:Key"]))
+                    };
+                })
+                ;
+
+
+
+
+            return services;
+        }
+    }
+}
